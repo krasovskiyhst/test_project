1adb574f6a09bd155dc870feeb4c72172a3cacdb
krasovskiyhst <79247009+krasovskiyhst@users.noreply.github.com>
2022-02-13 01:23:40 +0700

    config

--- test_project/settings.py
+++ test_project/settings.py
@@ -102,6 +102,15 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = '6379'
+CELERY_TIMEZONE = "Asia/Novosibirsk"
+CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_BROKER_TRANSPORT_OPTION = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
 try:
     from .local_settings import *
