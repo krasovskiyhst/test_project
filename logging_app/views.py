feb43ac7a3ebdafc8a7cdb3c11663778baca3802
krasovskiyhst <79247009+krasovskiyhst@users.noreply.github.com>
2022-02-13 19:51:53 +0700

    pages and authorization

--- logging_app/views.py
+++ logging_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .models import AccessLogs
 from .forms import DateForm, IPForm
 from django.contrib.auth.decorators import login_required
-from .tasks import parsing_logs
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
